56cea92066ae899522f17b7c93529b516ad9fa3c
Tushar <ec2-user@ip-192-168-100-202.ec2.internal>
2018-05-05 00:05:35 +0000

    Added pre build test to check SSM params exist

--- pre_build.py
+++ pre_build.py
@@ -3,6 +3,17 @@ import boto3
 import yaml
 import re
 
+def ssm_verify_param(client,params):
+    try:
+        response = client.get_parameters(Names=params)
+        if (response['InvalidParameters']):
+            return 1
+        return 0 
+    except Exception as e:
+        print (e)
+        return 1
+    
+    
 def main():
     with open("serverless.yml",'r') as svl_yaml:
         try:
@@ -11,15 +22,19 @@ def main():
             
             ssm_kms_arn = re.search(r'\${ssm:(.*?)(\~.*|})',svl_param['functions']['starter']['iamRoleStatements'][0]['Resource']).group(1)
             ssm_cw_event = re.search(r'\${ssm:(.*?)(\~.*|})',svl_param['functions']['starter']['events'][0]['schedule']['name']).group(1)
-            
             client_ssm = boto3.client('ssm')
-            print ssm_kms_arn, ssm_cw_event, ssm_kms_key
+            #print ssm_kms_arn, ssm_cw_event, ssm_kms_key
             
-            
-            return (True)
+            return ssm_verify_param(client_ssm,
+                [
+                ssm_kms_key,
+                ssm_kms_arn,
+                ssm_cw_event
+                ])
+                
         except  Exception as e :
             print (e)
-            return (False)
+            return 1
             
 if __name__ == '__main__':
         sys.exit(main())
