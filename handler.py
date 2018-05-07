cee350d703b31ec5b0bc838f3a45f8f8441d28fa
Tushar <ec2-user@ip-192-168-100-202.ec2.internal>
2018-05-07 14:35:47 +0000

    Implemented batch calls with max size as  999

--- handler.py
+++ handler.py
@@ -11,6 +11,7 @@ class Gmail():
     
     def __init__(self,credentials):
         self.credentials = credentials
+        self.batch_size = 999
         self.client_kms = boto3.client('kms')
         self.service = self.__build_service()
     
@@ -59,13 +60,11 @@ class Gmail():
 
 
     def mark_read(self,unread_messages,userid='me'):
-        if (len(unread_messages) >=999):
-            self.__mark_individual_read(unread_messages,userid)
-            
-        elif(unread_messages):
-            self.__mark_batch_read(unread_messages,userid)
-            
-        print ("Marked %s messages as Read" %len(unread_messages))   
+        while(len(unread_messages)>0):
+            unread_messages_slice = unread_messages[:self.batch_size]
+            self.__mark_batch_read(unread_messages_slice, userid)
+            unread_messages = unread_messages[self.batch_size:]
+        
     
     def __mark_individual_read(self,unread_messages,userid):
         for unread in unread_messages:
@@ -86,7 +85,7 @@ class Gmail():
             message = self.service.users().messages().batchModify(userId=userid,
                                             body=msg_labels).execute()
 
-            # print ("Marked %s messages as Read" %len(message_ids))
+            print ("Marked %s messages as Read" %len(message_ids))
         except errors.HttpError, error:
             print ('An error occurred: %s' % error)
 
